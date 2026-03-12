b799758f1e60c82d78e01c05f058c81139da9a2b
Josie Barber <barbe497@umn.edu>
2026-03-12 15:26:26 -0500

    Loop through scraped shifts to create calendar events
    Updates the scraping logic to return shift data and implements a loop to create Google Calendar events for each entry. Also enables headless mode for the web scraper and improves user-specific shift filtering during extraction.

--- src/calendar_converter.py
+++ src/calendar_converter.py
@@ -7,12 +7,18 @@ name = input("Enter your first name (capitalized first letter): ")
 username = input("Enter your Crew Quarters username: ")
 password = getpass.getpass("Enter your password (input hidden): ")
 
-scrape_schedule(username, password, name)
+shifts = scrape_schedule(username, password, name)
+print(shifts)
 print("Schedule scraping completed successfully. \n\n")
 
 calendar_name = input("Enter the wanted calendar name: ")
 
 service = get_calendar_service()
-emt_calendar_id = verify_calendar_exists(service, calendar_name)
+calendar_id = verify_calendar_exists(service, calendar_name)
 
-create_event(service, emt_calendar_id, datetime.date(2026, 3, 13), "AM", "First")
+
+for shift in shifts:
+    # print(shift)
+    # print("\n")
+    create_event(service, calendar_id, shift["date"], shift["time"], shift["shift"])
+# create_event(service, calendar_id, shifts["date"], shifts["time"], shifts["shift"])
